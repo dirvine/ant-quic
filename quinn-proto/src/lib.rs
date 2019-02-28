cih-y2k/quinn

Async API refactor

quinn-proto/src/lib.rs
@@ -24,7 +24,7 @@ mod transport_parameters;
 mod varint;
 
 mod connection;
-pub use crate::connection::{ConnectionError, ConnectionHandle, TimerUpdate};
+pub use crate::connection::{Connection, ConnectionError, ConnectionHandle, TimerUpdate};
 
 mod crypto;
 pub use crate::crypto::{ClientConfig, ConnectError, TokenKey};

dirvine/ant-quic

quinn-proto/src/lib.rs
@@ -24,7 +24,7 @@ mod transport_parameters;
 pub mod varint;
 
 mod connection;
-pub use crate::connection::{ConnectionError, TimerSetting, TimerUpdate};
+pub use crate::connection::{Connection, ConnectionError, TimerSetting, TimerUpdate};
 
 mod crypto;
 pub use crate::crypto::{ClientConfig, TokenKey};